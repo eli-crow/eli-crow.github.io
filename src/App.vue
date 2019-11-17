9404c36b725a31882d3b32652bfd205424ccd284
Eli Crow <elimichaelcrow@gmail.com>
2019-11-16 19:27:35 -0500

    Something notable

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="site">
+    <Crow class="interactive"/>
+    <div class="typography content">
+      <h1>Eli Crow</h1>
+      <p class="large">
+        <span class="line">is a designer and developer</span>
+        <span class="line">making software for business folk</span>
+        <span class="line">
+          at
+          <a class="link" href="https://www.sep.com/" target="_blank">sep.com</a>.
+        </span>
+      </p>
+    </div>
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Crow from "./components/Crow.vue";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Crow
   }
-}
+};
 </script>
 
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+:root {
+  --surface-0: #161616;
+  --surface-1: #262626;
+  --text: #afafaf;
+  --heading: #f4f4f4;
+  --blue: #1ed3c6;
+
+  --alverata: "Alverata", "Times New Roman", sans-serif;
+  --proxima-nova: "Proxima Nova", "Open Sans", "Gill Sans MT", "Gill Sans",
+    Corbel, Helvetica, Arial, sans-serif;
+}
+
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: var(--proxima-nova);
+  line-height: 1.3;
+  color: var(--text);
+  background-color: var(--surface-0);
+}
+
+.typography {
+}
+
+.typography h1 {
+  font-family: var(--alverata);
+  font-size: 56px;
+  font-weight: 700;
+  color: var(--heading);
+  margin-top: 120px;
+  margin-bottom: 12px;
+}
+
+.typography .large {
+  font-size: 24px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+.typography .line {
+  display: block;
+}
+
+.typography .link {
+  color: var(--blue);
+}
+
+.typography *:first-child {
+  margin-top: 0;   
+}
+
+.typography *:last-child {
+  margin-bottom: 0;   
+}
+</style>
+
+<style scoped>
+.site {
+  height: 100%;
+  width: 100%;
+  max-width: 600px;
+  max-height: 600px;
+  display: grid;
+  grid:
+    "top    top     top"     1fr
+    "left   content content" 1fr
+    "bottom bottom  bottom"  1fr
+  /  1fr    1fr     1fr;
+}
+
+.interactive {
+  grid-column: 1 / span 2;
+  grid-row: 1 / -1;
+}
+
+.content {
+  grid-area: content;
+  position: relative;
+  z-index: 1;
 }
 </style>
---
+++ src/components/Crow.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="Crow">
+    <canvas ref="canvas" class="canvas"></canvas>
+  </div>
+</template>
+
+
+
+<script>
+import * as PIXI from 'pixi.js'
+import SimplexNoise from 'simplex-noise'
+
+import textureSnake from '../assets/snake.png'
+import textureWings from '../assets/wings.png'
+
+export default {
+  mounted () {
+    const JOINT_LENGTH = 14
+    const JOINT_MAX_ANGLE_MAGNITUDE = Math.PI * 0.1
+    const JOINT_LERP_FACTOR = 0.98
+    const JOINT_ANGLE_LERP_FACTOR = 0.5
+
+    const noise = new SimplexNoise()
+
+    const app = new PIXI.Application({
+      view: this.$refs.canvas,
+      antialias: true,
+      autoDensity: true,
+      transparent: true,
+      resizeTo: this.$el,
+    })
+
+    const mouse = app.renderer.plugins.interaction.mouse.global
+
+    const snakePoints = Array.from({length: 100}, (_, i) => new PIXI.Point(i * JOINT_LENGTH, 0))
+    const snakePointsIncludingMouse = [...snakePoints, mouse]
+    const snake = new PIXI.SimpleRope(PIXI.Texture.from(textureSnake), snakePoints, 0);
+    snake.x = 0;
+    app.stage.addChild(snake)
+
+    const wings = new PIXI.Sprite(PIXI.Texture.from(textureWings))
+    app.stage.addChild(wings)
+    wings.anchor.set(0.5)
+
+    app.ticker.add(() => {
+
+      //enforce soft maximum angle magnitude constraints on the joints
+      for (let i = snakePointsIncludingMouse.length-2, ii = snakePointsIncludingMouse.length; i>=1; i--) {
+        const last = snakePointsIncludingMouse[i+1]
+        const curr = snakePointsIncludingMouse[i]
+        const next = snakePointsIncludingMouse[i-1]
+
+        const currXDiff = curr.x - last.x
+        const currYDiff = curr.y - last.y
+        const currLength = Math.sqrt(currXDiff**2 + currYDiff**2)
+        const nextXDiff = next.x - curr.x
+        const nextYDiff = next.y - curr.y
+        const nextLength = Math.sqrt(nextXDiff**2 + nextYDiff**2)
+        const currAngle = Math.atan2(currYDiff, currXDiff)
+        const nextAngle = Math.atan2(nextYDiff, nextXDiff)
+
+        let angleDifference = nextAngle - currAngle
+        angleDifference += (angleDifference>Math.PI) ? -2*Math.PI : (angleDifference<-Math.PI) ? 2*Math.PI : 0
+
+        if (i === 80) {
+          wings.position.copyFrom(curr)
+          wings.rotation = currAngle + Math.PI
+        }
+
+        let newAngle
+        if (angleDifference < -JOINT_MAX_ANGLE_MAGNITUDE) {
+          newAngle = currAngle - JOINT_MAX_ANGLE_MAGNITUDE
+        }
+        else if (angleDifference > JOINT_MAX_ANGLE_MAGNITUDE) {
+          newAngle = currAngle + JOINT_MAX_ANGLE_MAGNITUDE
+        } else {
+          continue
+        }
+        
+        const targetX = curr.x + Math.cos(newAngle) * nextLength
+        const targetY = curr.y + Math.sin(newAngle) * nextLength
+
+        next.x = next.x * (1-JOINT_ANGLE_LERP_FACTOR) + targetX * JOINT_ANGLE_LERP_FACTOR
+        next.y = next.y * (1-JOINT_ANGLE_LERP_FACTOR) + targetY * JOINT_ANGLE_LERP_FACTOR
+      } 
+
+      //force joints to be JOINT_LENGTH apart 
+      for (let i = snakePointsIncludingMouse.length-2, ii = snakePointsIncludingMouse.length; i>=0; i--) {
+        const last = snakePointsIncludingMouse[i+1]
+        const curr = snakePointsIncludingMouse[i]
+
+        const xDist = curr.x - last.x
+        const yDist = curr.y - last.y
+
+        const len = Math.sqrt(xDist ** 2 + yDist ** 2)
+
+        const xTarget = last.x + (xDist / len) * JOINT_LENGTH
+        const yTarget = last.y + (yDist / len) * JOINT_LENGTH
+
+        curr.x = curr.x * (1-JOINT_LERP_FACTOR) + xTarget * JOINT_LERP_FACTOR
+        curr.y = curr.y * (1-JOINT_LERP_FACTOR) + yTarget * JOINT_LERP_FACTOR
+        // curr.x = xTarget
+        // curr.y = yTarget
+      }
+    })
+  }
+}
+</script>
+
+
+
+<style scoped>
+.Crow {
+}
+</style>
